362c9c14aaf1ae79db7fca6ca4d4ecff09e7c14e
QuickResolve <lauranmontoya@gmail.com>
2016-05-15 17:50:41 -0700

    add hybrid view to local map

--- App/Components/Local.js
+++ App/Components/Local.js
@@ -6,21 +6,106 @@ import {
   MapView,
   ScrollView,
   TabBarIOS,
+  TouchableOpacity,
   View
 } from 'react-native';
 
 
-class Local extends Component {
+var Local = React.createClass ({
+  // watchID: (null: ?number),
+
+  getInitialState: function() {
+    return {
+      // initialPosition: 'unknown',
+      // lastPosition: 'unknown',
+      pin: {
+        latitude: 37,
+        longitude: -122
+      },
+      // isFirstLoad: true,
+      // mapRegion: undefined,
+      // mapRegionInput: undefined,
+      region: {
+        latitude: 37,
+        longitude: -122,
+        latitudeDelta: 2,
+        longitudeDelta: 2
+      }
+    };
+  },
+
+  componentDidMount: function() {
+    // navigator.geolocation.getCurrentPosition(
+    //   (position) => {
+    //     var initialPosition = JSON.stringify(position);
+    //     this.setState({initialPosition});
+    //   },
+    //   (error) => alert(error.message),
+    //   {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    // );
+    // this.watchID = navigator.geolocation.watchPosition((position) => {
+    //   var lastPosition = JSON.stringify(position);
+    //   this.setState({lastPosition});
+    // });
+    // this.setState({
+    //   pin: {
+    //     longitude: this.state.region.latitude,
+    //     latitude: this.state.region.longitude
+    //   }
+    // });
+  },
+
+
+
   render() {
     return(
       <View style={styles.container}>
-        <MapView style={styles.map}>
-
+        <MapView style={styles.map}
+          // annotations={this.state.pin}
+          showsUserLocation={true}
+          region={this.state.region}
+          mapType={'hybrid'}
+          followUserLocation={true}>
         </MapView>
       </View>
     )
   }
-};
+
+  // _getAnnotations(region) {
+  //   return [{
+  //     longitude: region.longitude,
+  //     latitude: region.latitude
+  //   }]
+  // },
+  //
+  // _onRegionChange(region) {
+  //   this.setState({
+  //     mapRegionInput: region
+  //   });
+  // },
+  //
+  // _onRegionChangeComplete(region) {
+  //   if (this.state.isFirstLoad) {
+  //     this.setState({
+  //       mapRegionInput: region,
+  //       annotations: this._getAnnotations(region),
+  //       isFirstLoad: false,
+  //     });
+  //   }
+  // },
+  //
+  // _onRegionInputChanged(region) {
+  //   this.setState({
+  //     mapRegion: region,
+  //     mapRegionInput: region,
+  //     annotations: this._getAnnotations(region)
+  //   });
+  // }
+
+});
+
+
+
 
 var styles = StyleSheet.create({
   container: {
